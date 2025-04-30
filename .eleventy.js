4550bdc1250e41b8d30260c814a246b9ffbd98bc
Corsin <corsinxhidber@gmail.com>
2025-04-30 16:44:13 +0200

    chore - adjusted config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,27 @@
+const { DateTime } = require("luxon"); // Require the Luxon library
+
 module.exports = function(eleventyConfig) {
-    // Passthrough Copy: Copy `css/` folder to `_site/css/`
-    eleventyConfig.addPassthroughCopy("css");
-  
-    // Return your Object options:
-    return {
-      dir: {
-        input: ".",      // Source files are in the root
-        includes: "_includes", // Folder for layouts, etc.
-        output: "_site"    // Output folder
-      }
-    };
+  // Passthrough Copy: Copy `css/` folder to `_site/css/`
+  eleventyConfig.addPassthroughCopy("css");
+
+  // Add readableDate filter
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    // Format date to 'dd LLL yyyy' (e.g., 27 Oct 2023)
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // Add htmlDateString filter
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    // Format date to 'yyyy-LL-dd' (e.g., 2023-10-27)
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
   };
+};
